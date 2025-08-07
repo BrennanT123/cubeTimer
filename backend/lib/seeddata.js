2d233cd3b66cb61cb8ed5a3e5af322806c8fcb0b
BrennanT123 <113391949+BrennanT123@users.noreply.github.com>
2025-08-06 23:18:00 -0400

    update seeding

--- backend/lib/seeddata.js
+++ backend/lib/seeddata.js
@@ -13,25 +13,32 @@ const prisma = new PrismaClient({
   },
 });
 
-async function createSolve(time,plusTwo,dnf,dateTime) {
+await prisma.solves.deleteMany({
+
+})
+
+async function createSolve(time, plusTwo, dnf, dateTime) {
   return await prisma.solves.create({
     data: {
-       time: parseFloat(time),
-       plusTwo,
-       dnf,
-       dateTime
+      time: parseFloat(time),
+      plusTwo,
+      dnf,
+      dateTime,
     },
   });
 }
 
-  const totalRecords = 100;
-  const now = new Date();
+const totalRecords = 1000;
+const now = new Date();
 
-  //calculate the start date: (100(total records) - 1) * 30s = 2970s = 49.5 min ago
-  let baseTime = new Date(now.getTime() - (totalRecords - 1) * 30 * 1000);
+//calculate the start date: (100(total records) - 1) * 30s = 2970s = 49.5 min ago
+let baseTime = new Date(now.getTime() - (totalRecords - 1) * 30 * 1000);
 
 for (let i = 0; i < totalRecords; i++) {
-  createSolve((Math.random() * 5 + 15).toFixed(2), Math.random() > .9, Math.random() >.95, baseTime);
+  const ideal = 15 + 75 * Math.exp(-i / (totalRecords/5));
+  const noise = (Math.random() - 0.5) * 10; 
+  const time = Math.max(ideal + noise, 10).toFixed(2);
+  createSolve(time, Math.random() > 0.9, Math.random() > 0.95, baseTime);
+  // console.log(time);
   baseTime = new Date(baseTime.getTime() + 30 * 1000);
-
 }
