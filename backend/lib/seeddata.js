e8d61360b383c4ab2560f54f1af87450403675c9
BrennanT123 <113391949+BrennanT123@users.noreply.github.com>
2025-08-08 09:40:00 -0400

    add scrambles to solves model

--- backend/lib/seeddata.js
+++ backend/lib/seeddata.js
@@ -17,16 +17,59 @@ await prisma.solves.deleteMany({
 
 })
 
-async function createSolve(time, plusTwo, dnf, dateTime) {
+async function createSolve(time, plusTwo, dnf, dateTime, scramble) {
   return await prisma.solves.create({
     data: {
       time: parseFloat(time),
       plusTwo,
       dnf,
       dateTime,
+      scramble,
     },
   });
 }
+
+
+
+  function createScramble() {
+    let scramble = "";
+    for (let i = 0; i < 16; i++) {
+      const numTurn = Math.floor(Math.random() * 6);
+      const numDirection = Math.round(Math.random());
+      let currentTurn;
+      switch (numTurn) {
+        case 0:
+          currentTurn = `F`;
+          break;
+        case 1:
+          currentTurn = `B`;
+          break;
+        case 2:
+          currentTurn = `D`;
+          break;
+        case 3:
+          currentTurn = `U`;
+          break;
+        case 4:
+          currentTurn = `L`;
+          break;
+        case 5:
+          currentTurn = `R`;
+          break;
+      }
+      switch (numDirection) {
+        case 0:
+          currentTurn = currentTurn + ` `;
+          break;
+        case 1:
+          currentTurn = currentTurn + `' `;
+      }
+      scramble = scramble + currentTurn;
+    }
+    return scramble;
+  }
+
+
 
 const totalRecords = 1000;
 const now = new Date();
@@ -38,7 +81,8 @@ for (let i = 0; i < totalRecords; i++) {
   const ideal = 15 + 75 * Math.exp(-i / (totalRecords/5));
   const noise = (Math.random() - 0.5) * 10; 
   const time = Math.max(ideal + noise, 10).toFixed(2);
-  createSolve(time, Math.random() > 0.9, Math.random() > 0.95, baseTime);
+  const scramble = createScramble();
+  createSolve(time, Math.random() > 0.9, Math.random() > 0.95, baseTime, scramble);
   // console.log(time);
   baseTime = new Date(baseTime.getTime() + 30 * 1000);
 }
