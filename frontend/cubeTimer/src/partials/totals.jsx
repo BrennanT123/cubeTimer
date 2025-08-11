288db3735957970ae20ef84c4c309ffab81d3284
BrennanT123 <113391949+BrennanT123@users.noreply.github.com>
2025-08-10 22:25:12 -0400

    update totals to respond to table changes

--- frontend/cubeTimer/src/partials/totals.jsx
+++ frontend/cubeTimer/src/partials/totals.jsx
@@ -3,37 +3,57 @@ import axios from "axios";
 import { API_LINK } from "../utl/constants";
 import { useEffect, useState, useRef } from "react";
 import { SolveChart } from "./chart";
+// import { useLocation } from "react-router-dom";
 
 export function Totals({ solves }) {
   const [numSolves, setNumSolves] = useState();
+  const [numDnf, setNumDnf] = useState(0);
+  const [numPlusTwo, setNumPlusTwo] = useState(0);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  //   const location = useLocation();
+  //   const solvesFromTable = location.state?.solves;
 
-  async function getTotalSolves() {
-    try {
-      setLoading(true);
-      const numberSolves = await axios.get(
-        `${API_LINK}/dataRouter/getNumSolves`,
-        {
-          withCredentials: true,
-        }
-      );
+  //   async function getTotalSolves() {
+  //     try {
+  //       setLoading(true);
+  //       const numberSolves = await axios.get(
+  //         `${API_LINK}/dataRouter/getNumSolves`,
+  //         {
+  //           withCredentials: true,
+  //         }
+  //       );
 
-      setNumSolves(numberSolves.data);
-      setLoading(false);
-      setLoading(false);
-    } catch (error) {
-      setError(error);
-    }
-  }
+  //       setNumSolves(numberSolves.data);
+  //       setLoading(false);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       setError(error);
+  //     }
+  //   }
 
   useEffect(() => {
-    getTotalSolves();
-  }, []);
+    setNumSolves(solves);
+    let tempDnf = 0;
+    solves.forEach((e) => {
+      if (e.dnf === true) {
+        tempDnf++;
+      }
+    });
+    let tempP2 = 0;
+    solves.forEach((e) => {
+      if (e.plusTwo === true) {
+        tempP2++;
+      }
+    });
+    setNumPlusTwo(tempP2);
+    setNumDnf(tempDnf);
 
-  function handleResetTotals() {
-    getTotalSolves();
-  }
+  }, [solves]);
+
+  //   function handleResetTotals() {
+  //     getTotalSolves();
+  //   }
 
   return (
     <>
@@ -43,21 +63,21 @@ export function Totals({ solves }) {
         <>
           <div className={dataStyles.solveCountTable}>
             <div>
-              Number of Solves:<div> {numSolves.solveCount}</div>
+              Number of Solves:<div> {solves.length}</div>
             </div>
             <div>
-              Number of DNFs: <div>{numSolves.dnfCount}</div>
+              Number of DNFs: <div>{numDnf}</div>
             </div>
             <div>
-              Number of +2s: <div>{numSolves.p2Count}</div>
+              Number of +2s: <div>{numPlusTwo}</div>
             </div>
           </div>
-          <button
+          {/* <button
             onClick={handleResetTotals}
             className={dataStyles.resetTotalButton}
           >
             Reset Totals
-          </button>
+          </button> */}
         </>
       )}
     </>
